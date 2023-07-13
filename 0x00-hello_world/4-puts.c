8abd4bbca89213ed4dd79f2c6e567c8bc13df847
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-13 23:39:53 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 * Prints what required.
+*@return returning integer value of function.
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 
-//returning the value of function
 return (0);
 }
