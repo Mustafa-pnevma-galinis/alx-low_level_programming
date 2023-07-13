80d0bbeef8d78614a2feaa726bba52ba98c9d9d2
Mustafa-pnevma-galinis <mustafa.sofias7099@gmail.com>
2023-07-13 14:05:27 -0700

    File#7

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - program
+* Return: always 0
+*/
+
+int main(void)
+{
+printf("Size of char: %d byte(s)",1);
+printf("\nSize of an int: %d byte(s)",4);
+printf("\nSize of a long int: %d byte(s)",4);
+printf("\nSize of a long long int: %d byte(s)",8);
+printf("\nSize of a float: %d byte(s)",4);
+
+return (0);
+}
