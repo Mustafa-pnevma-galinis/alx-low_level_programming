d1a7a5ec8e94436aa9688348dff57f9a1c0c1edc
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-14 00:06:49 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ printf("Size of char: %d byte(s)",1);
 printf("\nSize of an int: %d byte(s)",4);
 printf("\nSize of a long int: %d byte(s)",4);
 printf("\nSize of a long long int: %d byte(s)",8);
-printf("\nSize of a float: %d byte(s)",4);
+printf("\nSize of a float: %d byte(s)\n",4);
 
 return (0);
 }
