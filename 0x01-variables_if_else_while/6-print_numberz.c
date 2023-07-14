cbe2c53a77126b2dfb9bb3d50f05ca8fc7e48a86
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-15 02:28:28 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,19 +8,19 @@
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
-	int i = 48;
+	int i;
 
-	
+	i = 48;
+
 	while (i < 58)
 	{
 		putchar(i);
 		i++;
 	}
-	
+
 	putchar(10);
-	
+
 	return (0);
 }
