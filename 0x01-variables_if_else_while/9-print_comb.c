714322cb7bb63ee67d535b4213b7699d6ad95ee4
Mustafa-pnevma-galinis <mustafa.sofias7099@gmail.com>
2023-07-14 15:17:59 -0700

    Files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include<string.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+    int comma = 44;
+    int space = 32;
+
+    int number = 48;
+
+    int count = 0;
+
+    while (number < 58){
+
+        putchar(number);
+        if (number != 57){
+            putchar(comma);
+
+            putchar(space);
+        }
+
+        number++;
+
+    }
+	return (0);
+}
