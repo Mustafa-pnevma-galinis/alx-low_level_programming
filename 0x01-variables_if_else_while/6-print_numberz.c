81eb9f0c3887f96dd2e1e0b4225684183cdc9a98
Mustafa-pnevma-galinis <mustafa.sofias7099@gmail.com>
2023-07-14 14:20:27 -0700

    Files

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include<string.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i = 48;
+	while (i < 58){
+
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
