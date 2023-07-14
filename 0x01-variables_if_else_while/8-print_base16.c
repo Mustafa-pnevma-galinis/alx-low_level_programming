22dc9132ef748ef8c6229e2918679934d50f0db0
Mustafa-pnevma-galinis <mustafa.sofias7099@gmail.com>
2023-07-14 14:59:34 -0700

    Files

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include<string.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	 int cnt = 0;
+
+	 int decimalnumber = 0 + (10)*pow((double)16, (double)cnt);
+     //cnt++;
+     //printf("%x\n", 10);
+     printf("%d", decimalnumber);
+     //putchar(decimalnumber);
+	/*while (i > 96){
+
+		putchar(i);
+		i--;
+	}
+
+	putchar('\n');*/
+	return (0);
+}
