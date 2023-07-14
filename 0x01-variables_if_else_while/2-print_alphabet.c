1be87974d0a1757820ae2b4163d1e4892b6c9ef9
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-15 01:41:31 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,22 @@
 #include <time.h>
 #include <stdio.h>
 #include<string.h>
+
 /**
 * main - program
 * Return: always 0
 */
+
 int main(void)
 {
 	int i = 97;
 	
 	while (i < 123)
 	{
-	putchar(i);
-	i++;
+		putchar(i);
+		i++;
 	}
 	putchar('\n');
+	
 	return (0);
 }
