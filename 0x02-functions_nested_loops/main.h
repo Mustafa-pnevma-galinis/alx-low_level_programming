aff816b2b213d30b74a16c956df8e05f6dd31be2
Mustafa-pnevma-galinis <mustafa.sofias7099@gmail.com>
2023-07-18 14:44:37 -0700

    Adding

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-void _putchar();
-void print_alphabet();
-void 
-=======
 int _putchar(char ch);
 void print_alphabet(void);
->>>>>>> 349604b5cfbd4abeaed9d65b9f30ae21b3574fe1
+void print_alphabet_x10(void);
