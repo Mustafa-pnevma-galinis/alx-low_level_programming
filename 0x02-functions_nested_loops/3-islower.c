10e9551df73d0c2526f1ae890d90475d2ae7e99b
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-19 21:00:03 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,12 +8,10 @@
 */
 
 int _islower(int c){
-  int isLower(int c){
-            if (c < 97){
-                return 0;
-            }
-            else{
-                return 1;
-            }
-       }
+  if (c < 97){
+      return 0;
+  }
+  else{
+      return 1;
+          }
 }
