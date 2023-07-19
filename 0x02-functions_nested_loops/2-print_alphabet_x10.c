137ed2dd862861d65d6f3d7fc0151269fd77fd24
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-19 20:45:21 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-* print_alphabet - a function to print entire en alphabet in lowercase
+* print_alphabet_x10 - a function to print entire en alphabet in lowercase 10 times
 * 
 * Return: void
 */
@@ -13,21 +14,18 @@ void print_alphabet_x10(void){
         int val = 97;
         int count = 0;
 
-        while (val < 123){
-
+        while (val < 123)
+        {
             _putchar(val);
 
             val += 1;
 
-            if (val == 123 && count <= 8){
-
+            if (val == 123 && count <= 8)
+            {
                 count++;
                 val = 97;
                 _putchar('\n');
             }
-
         }
-
-        putchar('\n');
-          
+        putchar('\n');  
     }
