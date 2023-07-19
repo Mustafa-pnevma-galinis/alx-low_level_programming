8c2277dad5a2d07ce4a94acf8ab663135b928981
Mustafa-pnevma-galinis <mustafa.sofias7099@gmail.com>
2023-07-19 10:58:54 -0700

    File

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+* print_alphabet_x10 - a function to print entire en alphabet in lowercase 10 times
+* 
+* Return: void
+*/
+
+int _islower(int c){
+  int isLower(int c){
+            if (c < 97){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+       }
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char ch);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void _printf(char c[]);
+int _islower(int c);
