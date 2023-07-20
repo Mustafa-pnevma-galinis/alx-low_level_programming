66561f0012559f104ccdb34f891aacb93336692d
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-20 19:41:50 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,11 +6,14 @@
 * Return: 1 or 0.
 */
 
-int _isupper(int c){
-        if (c >= 65 && c <= 90){
-            return 1;
+int _isupper(int c)
+{
+        if (c >= 65 && c <= 90)
+        {
+            return (1);
         }
-        else {
-            return 0;
+        else 
+        {
+            return (0);
         }
-    }
+}
