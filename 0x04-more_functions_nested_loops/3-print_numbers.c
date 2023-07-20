93ba149e14c18a2181decdd6f5b0b00856dc734e
Mustafa-pnevma-galinis <mustafa.sofias7099@gmail.com>
2023-07-20 10:02:59 -0700

    Adding

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_numbers - return 1 if the provided character is uppercase and 0 if else.
+*
+* Return: void.
+*/
+
+void print_numbers(void){
+    for (int i = 48; i < 58; i++){
+            _putchar(i);
+        }
+        _putchar(10);
+}
