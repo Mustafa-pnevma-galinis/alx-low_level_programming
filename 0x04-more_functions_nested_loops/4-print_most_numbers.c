ce42c133ad65d7d6e697181508327d64da71ff69
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-20 21:13:41 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_numbers - return numbers except when equals 2 or 4.
+*
+* Return: void.
+*/
+
+void print_most_numbers(void)
+{
+    int i;
+    
+    for (i = '0'; i <= '9'; i++)
+    {
+      if (i == '2' || i == '4')
+      {
+          continue;  
+      }
+      _putchar(i);
+    }
+        _putchar('\n');
+}
