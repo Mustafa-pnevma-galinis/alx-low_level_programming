1d3304f117292d01cf08677d6ed60801beccae1a
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-21 00:27:50 +0300

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a line of _ n long
+ * @n: length of line
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+  int i, j;
+    for (i = 0; i < length; i++) {
+      for (j = 0; j < i; j++) {
+        printf(" ");
+      }
+      printf("\\");
+      printf("\n");
+    } int i, j;
+    for (i = 0; i < length; i++) {
+      for (j = 0; j < i; j++) {
+        printf(" ");
+      }
+      printf("\\");
+      printf("\n");
+    }
+}
