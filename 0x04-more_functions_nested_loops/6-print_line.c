61fa56d78bd9e6f8ffed9401832feda4ca21f9a7
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-20 21:43:44 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+void print_numbers(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+      if (n <= 0)
+      {
+        _putchar('\n');
+      }
+      else
+      {
+        _putchar(95);
+      }
+    }
+    _putchar('\n');
+}
