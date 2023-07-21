9213f905760b453b7f084c95875885dea1f0e59d
Mustafa Muhammad <67578577+Mustafa-pnevma-galinis@users.noreply.github.com>
2023-07-21 22:44:37 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,23 @@
 
 int main(void) 
 {
-  int i;
-  
-  for (i = 1; i < 101; i++) 
-  {
-      if (i % 5 == 0 && i % 3 == 0) 
-        printf("FizzBuzz ");
-      else if (i % 3 == 0) 
-        printf("Fizz ");
-      else if (i % 5 == 0) 
-          if (i == 100)
-            printf("Buzz");
-          else
-            printf("Buzz "); 
-      else 
-        printf("%d", i); 
-  }
-  printf("\n");
-
-  return (0);
+        int i;
+        
+        for (i = 1; i < 101; i++) 
+        {
+            if (i % 5 == 0 && i % 3 == 0) 
+              printf("FizzBuzz ");
+            else if (i % 3 == 0) 
+              printf("Fizz ");
+            else if (i % 5 == 0) 
+                if (i == 100)
+                  printf("Buzz");
+                else
+                  printf("Buzz "); 
+            else 
+              printf("%d", i); 
+        }
+        printf("\n");
+      
+        return (0);
 }
